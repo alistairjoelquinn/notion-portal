0d5fd32622bf6d76655eea9a0d4d9f3f0f837c63
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-07-29 10:20:52 +0200

    types added to structure of _document

--- src/models/_document.ts
+++ src/models/_document.ts
@@ -1,5 +1,9 @@
 import { NextPageContext } from 'next';
 
+type CallbackProps = (props: any) => any;
+
+type CallbackApp = (App: any) => CallbackProps;
+
 export interface GetPropsContext extends NextPageContext {
-    renderPage: (App: any) => any;
+    renderPage: (callback: CallbackApp) => any;
 }
